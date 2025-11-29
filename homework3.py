33edf92a2cc42398a79a70e3fc767f51f306da7c
inayaasf <inayahsafitri6@gmail.com>
2025-11-29 12:34:11 +0700

    upload semua tugas ddp

---
+++ homework3.py
@@ -0,0 +1,7 @@
+nama = "Inayah Safitri"
+umur = "18"
+prodi = "Sistem Informasi"
+nilai_UTS = float(92)
+
+print(f"""Halo {nama} dari jurusan {prodi}. 
+      Umur kamu {umur} tahun dan nilai UTS kamu {nilai_UTS}""")
